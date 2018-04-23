49727cae9ff5f8b0fbc0da9d9b882529f5e7f05d
lianghangbin <lianghangbin@weixinhai.com>
2018-04-23 23:22:57 +0800

    fix

--- led.js
+++ led.js
@@ -1,14 +1,21 @@
-var rpio = require('rpio');
-var pin = 12;
+const rpio = require('rpio');
+const pin = 12;
+
 rpio.open(pin, rpio.OUTPUT, rpio.LOW);
 
-for (var i = 0; i < 5; i++) {
-    rpio.write(12, rpio.HIGH);
-    rpio.sleep(1);
- 
-    rpio.write(12, rpio.LOW);
-    rpio.msleep(500);
+let flash = (count) => {
+    for(let i=0; i<count; i++) {
+        setTimeout(()=>{
+            rpio.write(pin, rpio.HIGH);
+            rpio.sleep(1);
+        
+            rpio.write(pin, rpio.LOW);
+            rpio.msleep(500);
+        }, count * 1000);
+    }
 }
+
+flash(30);
 
 var arguments = process.argv.splice(2);
 console.log('所传递的参数是：', arguments);
